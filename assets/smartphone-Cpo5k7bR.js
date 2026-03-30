7594f18407b78707b831de225d6b1c6b3b1e70fa
Manassembemba <Manassembemba@users.noreply.github.com>
2026-03-30 20:10:26 +0000

    deploy: 7caffd3050698f069c6c97fac3b16779ac8d9828

---
+++ assets/smartphone-Cpo5k7bR.js
@@ -0,0 +1,11 @@
+import{ay as e}from"./index-0yIKrPLt.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=e("CircleCheck",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m9 12 2 2 4-4",key:"dzmm74"}]]);/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=e("Smartphone",[["rect",{width:"14",height:"20",x:"5",y:"2",rx:"2",ry:"2",key:"1yt0o3"}],["path",{d:"M12 18h.01",key:"mhygvu"}]]);export{r as C,t as S};
